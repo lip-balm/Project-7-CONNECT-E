ee5b6f1e530218997829c47ee24bbedc3ad1557b
wyanda <justwannalearnok@gmail.com>
2022-11-27 23:17:34 -0500

    .

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,5 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
-const database = require('../database');
 
 class User extends Sequelize.Model {}
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
-const database = require('../database');
 const cors = require('cors');
 const bodyParser= require('body-parser');
 
